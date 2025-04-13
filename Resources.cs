d743128d373a450bbd8df6b8ba63363330f70825
米茶 <new1271@outlook.com>
2025-04-14 01:44:23 +0800

    加入專案檔，並將語言檔案轉移至 Data 子資料夾

---
+++ Resources.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace WitherTorch.Windows.Language
+{
+    public readonly struct ResourcesData
+    {
+        private readonly Assembly _assembly;
+        private readonly CultureInfo _culture;
+        private readonly string _resourceName;
+
+        public CultureInfo Culture
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _culture;
+        }
+
+        public ResourcesData(Assembly assembly, CultureInfo culture, string resourceName)
+        {
+            _assembly = assembly;
+            _culture = culture;
+            _resourceName = resourceName;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Stream OpenResourceStream()
+            => _assembly.GetManifestResourceStream(_resourceName)!;
+    }
+
+    public static class Resources
+    {
+        public static IEnumerable<ResourcesData> GetLanguageResources()
+        {
+            const string ResourceNameHeader = "WitherTorch.Windows.Language.Data.";
+            const string ResourceNameFooter = ".json";
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string[] names = assembly.GetManifestResourceNames();
+            if (names.Length == 0)
+                yield break;
+
+            foreach (string name in names)
+            {
+                if (!name.StartsWith(ResourceNameHeader) || !name.EndsWith(ResourceNameFooter))
+                    continue;
+                CultureInfo culture;
+                try
+                {
+                    culture = CultureInfo.GetCultureInfo(name.Substring(ResourceNameHeader.Length, 
+                        name.Length - ResourceNameHeader.Length - ResourceNameFooter.Length));
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                yield return new ResourcesData(assembly, culture, name);
+            }
+        }
+    }
+}
